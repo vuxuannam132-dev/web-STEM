637ea63c76f2d44796513904885b81e9cff8d8ae
vuxuannam132-dev <vuxuannam132@gmail.com>
2026-07-15 23:24:38 +0700

    fix: duplicate import in Footer.tsx breaking vercel build

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,8 +2,6 @@
 
 import React from 'react'
 import Image from 'next/image'
-import React from 'react'
-import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 import { useBrand } from '@/components/providers/BrandProvider'
 
